9b9e2bea926e3cf735d839c5434cbe42af5cd3e9
Martin Hafskjold Thoresen <martinhath@gmail.com>
2016-10-16 17:42:33 +0200

    Add reset for Car. What to do about ws?

--- pacman/Car.scala
+++ pacman/Car.scala
@@ -20,15 +20,20 @@ class Car(n: Int, k: Int) extends Module {
   // addresses to read? If we parametrize it,
   // we "hardcode" it into the cars, which may
   // be what we want.
-  var addrReg = Reg(UInt(0, width=addrWidth))
   var io = new Bundle {
     val data = UInt(width=n * k).asInput
     val addr = UInt(width=addrWidth).asOutput
     val weights = Vec.fill(n){ UInt(width=k) }.asOutput
   }
 
-  addrReg += UInt(n * k)
+  var addrReg = Reg(UInt(0, width=addrWidth))
+  when (reset) {
+      addrReg := UInt(0)
+    } .otherwise {
+      addrReg := addrReg + UInt(n * k)
+    }
   io.addr := addrReg
+
   // TODO: if `k` is large, the runners may not be able to
   // calculate the entire sum dotproduct in one cycle?
   // If so, we might need to wait for some cycles, before
@@ -41,18 +46,23 @@ class Car(n: Int, k: Int) extends Module {
 }
 
 class CarTests(c: Car, n: Int, k: Int) extends Tester(c) {
-  val randomNums = (0 until n).map((_) => rnd.nextInt(math.pow(2, k).toInt))
-
   // Set the `n` chunks to be random `k` bits
+  val randomNums = (0 until n).map((_) => rnd.nextInt(math.pow(2, k).toInt))
   var dataInput = 0
   for (i <- 0 until n) {
     dataInput |= randomNums(i) << i * k
   }
   poke(c.io.data, dataInput)
+
   step(1)
+
   expect(c.io.addr, n * k)
   for (i <- 0 until n) {
     expect(c.io.weights(i), randomNums(i))
   }
+  poke(c.reset, 1);
+
   step(1)
+
+  expect(c.io.addr, 0);
 }
