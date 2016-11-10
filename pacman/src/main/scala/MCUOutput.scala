72b25aa6df94dd919e2011ab611c13626dbbc6ce
Jon Andreas Melbye <jonandme@stud.ntnu.no>
2016-11-10 11:11:21 +0100

    Added modified MCU<->FPGA protocol.

--- pacman/src/main/scala/MCUOutput.scala
+++ pacman/src/main/scala/MCUOutput.scala
@@ -18,13 +18,16 @@ class MCUOutput/*(parameters: LayerParameters )*/ extends Module {
   val outCtrl = Module(new MCUOutCtrl)
   val data = OHToUInt(io.oneHotIn.bits)
   val rdyAch = Reg(init=Bool(true))
-  
+  val valid = Reg(init=Bool(false))
+
   // Output 
   io.ebiD := Cat(outBuff(0), outBuff(1), outBuff(2), outBuff(3))
   io.oneHotIn.ready := outCtrl.io.addr =/= UInt(15)
+  valid := io.ebiReEn && outCtrl.io.state(3)===Bits(1) //TODO: determine if more delay is needed
+  io.ebiValid := valid
   
   // Control Input
-  when (io.achRead) { rdyAch := io.ebiReEn}
+  when (io.achRead && valid) { rdyAch := io.achRead}
   outCtrl.io.valid := io.oneHotIn.valid
   outCtrl.io.fillIn := rdyAch
    
