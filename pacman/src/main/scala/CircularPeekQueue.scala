7606ebaf0074f1f40543a6d5a841ea6ba22a410c
Nils Barlaug <nils.barlaug@gmail.com>
2016-11-10 20:20:48 +0100

    Add CircularPeekQueue

--- pacman/src/main/scala/CircularPeekQueue.scala
+++ pacman/src/main/scala/CircularPeekQueue.scala
@@ -6,25 +6,7 @@ import fpgatidbits.ocm.DualPortBRAM
 
 class CircularPeekQueue(blockSize: Int, numberOfBlocks: Int, dataWidth: Int) extends Module {
 
-  class ResetCounter(start: Int, max: Int, step: Int = 1) extends Module {
-    val io = new Bundle {
-      val enable = Bool().asInput
-      val writeEnable = Bool().asInput
-      val writeData = Bool().asInput
-      val value = UInt().asOutput
-    }
-
-    val startValue = UInt(start, width=UInt(max - 1).getWidth)
-    val v = Reg(init = startValue)
-    when (io.writeEnable) {
-      v := io.writeData
-    } .otherwise {
-      v := Mux(io.enable, v + UInt(step), v)
-    }
-    io.value := v
-  }
-
-  val addressWidth = (Math.log(blockSize * numberOfBlocks) / Math.log(2)).ceil.toInt
+  val addressWidth = (Math.log(blockSize.toDouble * numberOfBlocks) / Math.log(2)).ceil.toInt
   val io = new Bundle {
     val writeEnable = Bool().asInput
     val nextBlock = Bool().asInput
@@ -32,17 +14,17 @@ class CircularPeekQueue(blockSize: Int, numberOfBlocks: Int, dataWidth: Int) ext
     val output = Bits(width=dataWidth).asOutput
   }
 
-  val currentBlockOffset = Module(new Counter(0, blockSize * numberOfBlocks, step=blockSize))
-  currentBlockOffset.io.enable  := io.nextBlock
-  currentBlockOffset.io.rst     := currentBlockOffset.io.value === UInt(blockSize * numberOfBlocks)
-  val inBlockReadOffset = Module(new ResetCounter(0, blockSize))
+  val currentBlockOffset = Module(new AsyncCounter(0, blockSize * numberOfBlocks, blockSize))
+  currentBlockOffset.io.enable := io.nextBlock
+
+  val inBlockReadOffset = Module(new CounterWithSyncAndAsyncReset(0, blockSize))
   inBlockReadOffset.io.enable := Bool(true)
-  inBlockReadOffset.io.writeEnable    := io.nextBlock || inBlockReadOffset.io.value === UInt(blockSize - 1)
-  inBlockReadOffset.io.writeData := Mux(io.nextBlock, UInt(1), UInt(0))
+  inBlockReadOffset.io.asyncRst := io.nextBlock
+  inBlockReadOffset.io.syncRst := inBlockReadOffset.io.value === UInt(blockSize -  1)
 
   val writeAddr = Reg(init=UInt(blockSize, width=UInt(blockSize * numberOfBlocks).getWidth))
   when (io.writeEnable) {
-    when (writeAddr === UInt(blockSize * numberOfBlocks)) {
+    when (writeAddr === UInt(blockSize * numberOfBlocks - 1)) {
       writeAddr := UInt(0)
     } .otherwise {
       writeAddr := writeAddr + UInt(1)
