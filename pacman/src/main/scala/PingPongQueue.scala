37e4860a12392e1964fc55a8d77c039ed7684ca4
Ulrich Johan Isachsen <ulrichji@stud.ntnu.no>
2016-11-14 15:25:54 +0100

    Fixed compilation error

--- pacman/src/main/scala/PingPongQueue.scala
+++ pacman/src/main/scala/PingPongQueue.scala
@@ -2,7 +2,7 @@ package Pacman
 
 import Chisel._
 
-class PingPongQueue(data_width : Int, queue_size : Int) extends module
+class PingPongQueue(data_width : Int, queue_size : Int) extends Module
 {
 	val io = new Bundle
 	{
@@ -17,11 +17,11 @@ class PingPongQueue(data_width : Int, queue_size : Int) extends module
 	
 	val adr_reg = Reg(init = UInt(0,log2Up(queue_size)))
 	val adr_reg_incremented = adr_reg + UInt(1)
-	val at_end = adr_reg === queue_size - 1	
-	val enable_count = io.valid || (!io.write_en)
+	val at_end = adr_reg === UInt(queue_size - 1)	
+	val enable_count = io.valid_trans || (!io.write_en)
 	val memory = Mem(UInt(width = data_width), queue_size, true) //TODO might not be sequential read
 
-	when(reset || at_end)
+	when(io.reset || at_end)
 	{
 		adr_reg := UInt(0)
 	}
