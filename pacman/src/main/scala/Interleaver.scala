fe13d3fd988cff7c0dda737df529d0bbfba16666
jonandme <jonandme@stud.ntnu.no>
2016-11-19 15:24:00 +0100

    Fixed the Interleaver to match flipped input.

--- pacman/src/main/scala/Interleaver.scala
+++ pacman/src/main/scala/Interleaver.scala
@@ -13,10 +13,10 @@ class Interleaver(parameters: LayerParameters) extends Module {
   val regsBuf = Vec.fill(parameters.NumberOfCores)(Reg(init=Bits(0,width=parameters.MatrixHeight )))
   val doneDelay = Reg(init=Bool(false))
   val send = Reg(init=Bool(false))
-  val count = Reg(init=UInt(0,width=log2Up(parameters.NumberOfCores)))
+  val count = Reg(init=UInt(parameters.NumberOfCores-1,width=log2Up(parameters.NumberOfCores+1)))
   if (parameters.NumberOfCores != 1){
     when(send && io.oneHotOut.ready){
-      count :=count+UInt(1)
+      count :=count-UInt(1)
     }
   } 
   for ( i <- 0 until parameters.NumberOfCores) {
@@ -27,8 +27,11 @@ class Interleaver(parameters: LayerParameters) extends Module {
     } 
   }
   doneDelay := io.doneIn
-  when(doneDelay || count===UInt(parameters.NumberOfCores-1)) {
+  when(doneDelay || count===UInt(0)) {
     send := doneDelay
+  }
+  when(send && count===UInt(0)){
+   count := UInt(parameters.NumberOfCores-1)
   }
   io.oneBitPerCore.ready := io.oneHotOut.ready
   io.oneHotOut.bits := regsBuf(count)
