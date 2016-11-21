81e3c092141bded986f4b06f384740fa179dbedb
Ulrich Johan Isachsen <ulrichji@stud.ntnu.no>
2016-11-21 10:23:05 +0100

    Refresh some vivado files

--- usbtest/vivado_proj/ulpi_test/ulpi_test.runs/impl_1/top.tcl
+++ usbtest/vivado_proj/ulpi_test/ulpi_test.runs/impl_1/top.tcl
@@ -42,89 +42,17 @@ proc step_failed { step } {
   close $ch
 }
 
-set_msg_config -id {Common 17-41} -limit 10000000
 set_msg_config -id {HDL 9-1061} -limit 100000
 set_msg_config -id {HDL 9-1654} -limit 100000
-set_msg_config -id {Synth 8-256} -limit 10000
-set_msg_config -id {Synth 8-638} -limit 10000
-
-start_step init_design
-set rc [catch {
-  create_msg_db init_design.pb
-  set_param xicom.use_bs_reader 1
-  reset_param project.defaultXPMLibraries 
-  open_checkpoint /home/ulrich/chisel/fpga/usbtest/vivado_proj/ulpi_test/ulpi_test.runs/impl_1/top.dcp
-  set_property webtalk.parent_dir /home/ulrich/chisel/fpga/usbtest/vivado_proj/ulpi_test/ulpi_test.cache/wt [current_project]
-  set_property parent.project_path /home/ulrich/chisel/fpga/usbtest/vivado_proj/ulpi_test/ulpi_test.xpr [current_project]
-  set_property ip_repo_paths /home/ulrich/chisel/fpga/usbtest/vivado_proj/ulpi_test/ulpi_test.cache/ip [current_project]
-  set_property ip_output_repo /home/ulrich/chisel/fpga/usbtest/vivado_proj/ulpi_test/ulpi_test.cache/ip [current_project]
-  write_hwdef -file top.hwdef
-  close_msg_db -file init_design.pb
-} RESULT]
-if {$rc} {
-  step_failed init_design
-  return -code error $RESULT
-} else {
-  end_step init_design
-}
-
-start_step opt_design
-set rc [catch {
-  create_msg_db opt_design.pb
-  opt_design 
-  write_checkpoint -force top_opt.dcp
-  report_drc -file top_drc_opted.rpt
-  close_msg_db -file opt_design.pb
-} RESULT]
-if {$rc} {
-  step_failed opt_design
-  return -code error $RESULT
-} else {
-  end_step opt_design
-}
-
-start_step place_design
-set rc [catch {
-  create_msg_db place_design.pb
-  implement_debug_core 
-  place_design 
-  write_checkpoint -force top_placed.dcp
-  report_io -file top_io_placed.rpt
-  report_utilization -file top_utilization_placed.rpt -pb top_utilization_placed.pb
-  report_control_sets -verbose -file top_control_sets_placed.rpt
-  close_msg_db -file place_design.pb
-} RESULT]
-if {$rc} {
-  step_failed place_design
-  return -code error $RESULT
-} else {
-  end_step place_design
-}
-
-start_step route_design
-set rc [catch {
-  create_msg_db route_design.pb
-  route_design 
-  write_checkpoint -force top_routed.dcp
-  report_drc -file top_drc_routed.rpt -pb top_drc_routed.pb
-  report_timing_summary -warn_on_violation -max_paths 10 -file top_timing_summary_routed.rpt -rpx top_timing_summary_routed.rpx
-  report_power -file top_power_routed.rpt -pb top_power_summary_routed.pb -rpx top_power_routed.rpx
-  report_route_status -file top_route_status.rpt -pb top_route_status.pb
-  report_clock_utilization -file top_clock_utilization_routed.rpt
-  close_msg_db -file route_design.pb
-} RESULT]
-if {$rc} {
-  step_failed route_design
-  return -code error $RESULT
-} else {
-  end_step route_design
-}
 
 start_step write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
+  set_param xicom.use_bs_reader 1
+  open_checkpoint top_routed.dcp
+  set_property webtalk.parent_dir /home/ulrich/chisel/fpga/usbtest/vivado_proj/ulpi_test/ulpi_test.cache/wt [current_project]
   catch { write_mem_info -force top.mmi }
-  write_bitstream -force top.bit 
+  write_bitstream -force top.bit -bin_file
   catch { write_sysdef -hwdef top.hwdef -bitfile top.bit -meminfo top.mmi -file top.sysdef }
   catch {write_debug_probes -quiet -force debug_nets}
   close_msg_db -file write_bitstream.pb
